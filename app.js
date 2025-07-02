25f958f5f6b93a271e6fc59d12c5d98dce06ac6a
Sagar Saini <sagarsaini005@gmail.com>
2025-07-02 23:57:37 +0530

    ch1- basic skeleton ready

---
+++ app.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1",{id:"heading"},"Hello ReactEats :)");
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(heading);
